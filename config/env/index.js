c01034c21c3ed1c7cae0bd623960a71f2d747a34
edwintantawi <edwintantawi@gmail.com>
2021-11-08 09:05:58 +0700

    🔧 Add getEnv function

---
+++ config/env/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const getEnv = (name) => {
+  const env = process.env[name];
+  if (env === undefined) throw new Error(`[.env] process.env.${name} not found`);
+  return env;
+};
+
+module.exports = { getEnv };
