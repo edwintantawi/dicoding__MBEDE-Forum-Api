de7cd795542c0ae82b50943082500f54e89d61bb
edwintantawi <edwintantawi@gmail.com>
2021-11-11 10:25:40 +0700

    🚀 Add container depedency and register extension

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,15 +1,17 @@
 const Hapi = require('@hapi/hapi');
 const { getEnv } = require('../../../config/env');
 const { usersPlugin } = require('../../Interfaces/http/api/users');
-const { container } = require('../container');
+const { extensionsPlugin } = require('../../Interfaces/http/extensions');
 
-const createServer = async () => {
+const createServer = async ({ container }) => {
   const server = Hapi.server({
     host: getEnv('HOST'),
     port: getEnv('PORT'),
   });
 
   server.register([{ plugin: usersPlugin, options: { container } }]);
+
+  server.register([{ plugin: extensionsPlugin }]);
 
   return server;
 };
