722b8e343bf1813936bbd346d14e703f6f4afb20
edwintantawi <edwintantawi@gmail.com>
2021-12-12 00:18:45 +0700

    🚀 Like helper

---
+++ tests/LikesTableTestHelper.js
@@ -0,0 +1,40 @@
+/* istanbul ignore file */
+
+const { pool } = require('../src/Infrastructures/database/postgres/pool');
+
+const LikesTableTestHelper = {
+  async addLike({
+    id = 'like-123',
+    commentId = 'comment-123',
+    userId = 'user-123',
+  }) {
+    const query = {
+      text: `INSERT INTO likes
+              VALUES($1, $2, $3)
+              RETURNING id`,
+      values: [id, commentId, userId],
+    };
+
+    const { rows } = await pool.query(query);
+    return rows[0].id;
+  },
+
+  async findLikeById(id) {
+    const query = {
+      text: `SELECT *
+              FROM likes
+              WHERE id = $1`,
+      values: [id],
+    };
+
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async cleanTable() {
+    await pool.query(`DELETE FROM likes
+                      WHERE 1=1`);
+  },
+};
+
+module.exports = { LikesTableTestHelper };
